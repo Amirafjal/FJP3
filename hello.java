5403b0f9aca9adc35b5def59250450b050391cd8
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2021-11-21 11:37:51 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+/**
+ * hello
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
